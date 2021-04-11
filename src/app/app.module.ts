2f48a80189d41603dfbcb4c03ad2c527979a526c
IHruch <ihruch.test@gmail.com>
2021-04-11 14:21:56 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { ICarOwnersServiceService } from './shared/services/ICarOwnersService.service'
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ICarOwnersServiceService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shared/types/ICarOwnersService.interface.ts
@@ -0,0 +1,16 @@
+import { Observable } from 'rxjs'
+import { OwnerEntityInterface } from './OwnerEntity.interface'
+import { CarEntityInterface } from './CarEntity.interface'
+
+export interface ICarOwnersServiceInterface {
+  getOwners(): Observable<OwnerEntityInterface[]>
+  getOwnerById(aId: number): Observable<OwnerEntityInterface>
+  createOwner(
+    aLastName: string,
+    aFirstName: string,
+    aMiddleName: string,
+    aCars: CarEntityInterface[]
+  ): Observable<OwnerEntityInterface>
+  editOwner(aOwner: OwnerEntityInterface): Observable<OwnerEntityInterface>
+  deleteOwner(aOwnerId: number): void
+}
